915a9e9cadf69faeb8483b615f57b35a725c981f
Justin Lupica <justinlupica@gmail.com>
2021-04-18 14:35:13 -0400

    Basic search functionality and basic styling

--- src/App.js
+++ src/App.js
@@ -6,30 +6,43 @@ const App = () => {
 
   const API_KEY = '142b402f15804516a38835cec56b9b54';
 
-  const [recipes, setRecipes] = useState([])
-
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState("chicken")
 
   useEffect(() => {
     getRecipes();
-  }, [])
+  }, [query])
   
-  const getRecipes = () => {fetch(`https://api.spoonacular.com/recipes/complexSearch?query=pasta&apiKey=${API_KEY}`)
+  const getRecipes = () => {fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${API_KEY}`)
     .then(res => res.json())
     .then(data => {
       console.log(data.results)
       setRecipes(data.results)
     })};
     
+  const updateSearch = e => {
+    setSearch(e.target.value)
+  }
+
+  const getSearch = e => {
+    e.preventDefault();
+    console.log(query)
+    setQuery(search)
+    setSearch('')
+  }
 
   return (
     <div className='App'>
-      <form className="search-form">
-        <input type="text" className="search-bar"/>
+      <form className="search-form" onSubmit={getSearch}>
+        <input type="text" className="search-bar" value={search} onChange={updateSearch}/>
         <button type="submit" className="search-button">Search</button>
       </form>
+      <div className='recipes'>
       {recipes.map(recipe => (
         <Recipe key={recipe.title} title={recipe.title} image={recipe.image}/>
       ))}
+      </div>
     </div>
   )
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import style from './recipe.module.css'
 
 const Recipe = ({title, image}) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <img src={image} alt=""/>
-        </div>
+      <div className={style.recipe}>
+        <h1>{title}</h1>
+        <img src={image} alt=""/>
+      </div>
     );
 }
 
